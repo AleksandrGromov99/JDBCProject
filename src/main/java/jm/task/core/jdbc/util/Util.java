7b2aeea1a6960b6dabf3f81695ae0bf2ce712918
AleksandrGromov99 <163206082+AleksandrGromov99@users.noreply.github.com>
2024-03-27 22:58:00 +0300

    Update Util.java

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,26 +3,18 @@ package jm.task.core.jdbc.util;
 import java.sql.*;
 
 public class Util {
-    // реализуйте настройку соединения с БД
     private static final String url = "jdbc:mysql://localhost:3306/mydb";
     private static final String user = "root";
     private static final String password = "root";
 
-
-    private static Statement statement;
-    private static ResultSet resultSet;
-
-    public static Connection getConnection(){
+    public static Connection getConnection() {
         Connection connection = null;
         try {
-            connection=DriverManager.getConnection(url,user,password);
+            connection = DriverManager.getConnection(url, user, password);
             System.out.println("Connection established");
         } catch (SQLException e) {
-            e.printStackTrace();
-            System.out.println("Connection not established");
+            throw new RuntimeException("Connection not established");
         }
         return connection;
     }
-//
-//    statement= connection.createStatement();
 }
